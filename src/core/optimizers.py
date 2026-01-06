35737f05c72e3883f74789aef834353002f9fff7
Nic Bonetto <nick.bonetto@gmail.com>
2026-01-06 12:14:00 -0800

    feat: added `sgd`, `momentumsgd`, and `rmsprop` optimization functions

--- src/core/optimizers.py
+++ src/core/optimizers.py
@@ -1,7 +1,64 @@
 import numpy as np
 
+class SGD:
+    def __init__(self, learning_rate=0.01):
+        self.learning_rate = learning_rate
+        self.history = []
+
+    def step(self, point, gradient):
+        new_point = point - self.learning_rate * gradient
+        self.history.append(new_point.copy())
+        return new_point
+    
+    def reset(self):
+        self.history = []
+
+class MomentumSGD:
+    def __init__(self, learning_rate=0.01, momentum=0.9):
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.velocity = None
+        self.history = []
+
+    def step(self, point, gradient):
+        if self.velocity is None:
+            self.velocity = np.zeros_like(point)
+
+        self.velocity = self.momentum * self.velocity - self.learning_rate * gradient
+        new_point = point + self.velocity
+
+        self.history.append(new_point.copy())
+        return new_point
+
+    def reset(self):
+        self.velocity = None
+        self.history = []
+
+class RMSProp:
+    def __init__(self, learning_rate=0.01, decay=0.9, epsilon=1e-8):
+        self.learning_rate = learning_rate
+        self.decay = decay
+        self.epsilon = epsilon
+        self.squared_grad = None
+        self.history = []
+
+    def step(self, point, gradient):
+        if self.squared_grad is None:
+            self.squared_grad = np.zeros_like(point)
+
+        self.squared_grad = (self.decay * self.squared_grad + (1 - self.decay) * gradient**2)
+        new_point = point - self.learning_rate * gradient / (self.squared_grad**0.5 + self.epsilon)
+
+        self.history.append(new_point.copy())
+        return new_point
+
+    def reset(self):
+        self.history = []
+        self.squared_grad = None
+
+
 class Adam:
-    def __init__(self, learning_rate=0.1, beta1=0.9, beta2=0.999, epsilon=1e-8):
+    def __init__(self, learning_rate=0.01, beta1=0.9, beta2=0.999, epsilon=1e-8):
         self.learning_rate = learning_rate
         self.beta1 = beta1
         self.beta2 = beta2
@@ -33,9 +90,12 @@ class Adam:
         self.v = None
         self.t = 0
 
-def get_optimizer(name, learning_rate=0.1):
+def get_optimizer(name, learning_rate=0.01):
     optimizers = {
-        'adam': Adam(learning_rate)
+        'adam': Adam(learning_rate),
+        'sgd': SGD(learning_rate),
+        'momentumsgd': MomentumSGD(learning_rate),
+        'rmsprop': RMSProp(learning_rate)
     }
     return optimizers[name.lower()]
 
